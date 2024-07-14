47429ee028695fd0631cda690ed1705b5e3d2870
Vasudevshetty <vasudeepu2815@gmail.com>
2024-07-14 11:57:15 +0530

    Loader component added

---
+++ client/src/pages/app/Loader.jsx
@@ -0,0 +1,10 @@
+// Loader.js
+import styles from "./styles/app.module.css";
+
+const Loader = () => (
+  <div className={styles.loader}>
+    <div className={styles.spinner}></div>
+  </div>
+);
+
+export default Loader;
