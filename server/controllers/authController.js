0c4e57e24c700d93df46d775d004970ba3ba9e5b
Vasudevshetty <vasudeepu2815@gmail.com>
2024-07-26 15:25:20 +0530

    Made it as secure cookie due to production

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -21,6 +21,7 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: true,
     sameSite: "none",
   };
 
