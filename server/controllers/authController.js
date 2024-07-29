81981f6448dca635a1b23c8b7fca2fc87405801a
Vasudevshetty <vasudeepu2815@gmail.com>
2024-07-29 20:17:35 +0530

    Checking logout functionality on cors

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -234,6 +234,8 @@ exports.logout = (req, res) => {
   res.cookie("jwt", "loggedout", {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
+    sameSite: "None",
+    secure: true,
   });
   req.user = undefined;
   res.status(200).json({ status: "success" });
